cd1f22b45b161ab72822a3f26af1c149e39b41a4
Tekraj15 <tekrajawasthi15@gmail.com>
2025-12-03 21:08:40 +0100

    Added loger for weight and bias

---
+++ src/utils/logger.py
@@ -0,0 +1,15 @@
+import wandb
+import logging
+from datetime import datetime
+
+def get_logger(name: str):
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.INFO)
+    return logger
+
+def init_wandb(cfg):
+    wandb.init(
+        project="lora-minisam",
+        name=f"{cfg.train.mode}_{datetime.now().strftime('%H%M')}",
+        config=OmegaConf.to_container(cfg, resolve=True)
+    )
